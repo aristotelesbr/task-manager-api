37bd5882b4b3c3b75382a77a5b3ae37e386dd464
aristotelesbr <aristotelesbr2014@gmail.com>
2017-07-09 14:27:38 +0000

    Adding destroy action to the tasks controller with tests

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -122,6 +122,23 @@ RSpec.describe 'Task API' do
       it 'does not updates the task in the database' do
         expect( Task.find_by(title: task_params[:title]) ).to be_nil
       end
+    end 
+  end
+
+  describe 'DELETE /task/:id' do
+    let!(:task) { create(:task, user_id: user.id) }
+
+    before do
+      delete "/tasks/#{task.id}", params: {}, headers: headers
+    end
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+
+    it 'removes the task from the database' do
+      expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      # expect( Task.find_by(id: task.id) ).to be_nil
     end
   end
 end
