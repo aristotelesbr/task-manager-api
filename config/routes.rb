83740c2abbd7aeb78e459b2313e58f7c21d87a1b
Aristoteles <aristotelesbr2014@gmail.com>
2017-08-25 15:49:36 -0300

    bugfix api_version_contranint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  devise_for :users, only: [:sessions], controller: { sessions: 'api/v1/sessions' }
+  devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
       resources :sessions, only: %i[create destroy]
       resources :tasks, only: %i[index show create update destroy]
     end
+
     namespace :v2, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do
       resources :users, only: %i[show create update destroy]
       resources :sessions, only: %i[create destroy]
